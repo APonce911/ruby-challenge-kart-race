ff64539b6b78cfeefc2e0b300edd62dd5cf86faa
Airton Ponce <airtonponce@outlook.com>
2019-01-18 17:34:28 -0200

    tests for parse_data method

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -4,11 +4,25 @@ describe 'The method race_winner:' do
   it 'Is not nil' do
     expect(race_winner).not_to be_nil
   end
-
   it 'Is not empty' do
     expect(race_winner).not_to eq('')
   end
   it 'Returns a string' do
     expect(race_winner).to be_a String
   end
+end
+
+describe 'The method parse_data:' do
+  it 'Is not nil' do
+    expect(parse_data).not_to be_nil
+  end
+  it 'Is not empty' do
+    expect(parse_data).not_to eq('')
+  end
+  it 'Returns an array' do
+    expect(parse_data).to be_a Array
+  end
+  # it 'First element returns a hash' do
+  #   expect(parse_data[0]).to be_a Hash
+  # end
 end

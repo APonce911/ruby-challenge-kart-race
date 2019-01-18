2908f2158272010b7cb9934c0b1de221535fd9da
Airton Ponce <airtonponce@outlook.com>
2019-01-18 20:05:17 -0200

    update Race model

--- lib/race.rb
+++ lib/race.rb
@@ -1,9 +1,9 @@
-# this class will be responsable of creating all instances of Lap objects
 require 'csv'
 require_relative 'lap'
+# this class will be responsable of creating all instances of Lap objects
 class Race
   def initialize
-    @laps = []
+    # @laps = []
     parse_data
   end
 
@@ -16,6 +16,7 @@ class Race
   private
 
   def parse_data
+    @laps = []
     file = './race.csv'
     csv_options = { col_sep: ',', quote_char: '"', headers: :first_row.to_sym }
     CSV.foreach(file, csv_options) { |row| @laps << { hour: row['hour'], cod: row['cod'].to_i, pilot: row['pilot'], lap_number: row['lap_number'], lap_time: row['lap_time'], lap_avg_time: row['lap_avg_time'] } }
@@ -23,8 +24,10 @@ class Race
     # add_lap(@laps)
   end
 
-# def add_lap
-#   p @laps.class
+  # def add_lap
+  #   p @laps.class
 
-# end
+  # end
 end
+
+# r = Race.new

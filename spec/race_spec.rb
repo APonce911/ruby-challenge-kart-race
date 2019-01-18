b4019e9b90180f624046ed7012426631ba83c4c1
Airton Ponce <airtonponce@outlook.com>
2019-01-18 13:49:34 -0200

    add 'Is not empty' test

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -5,6 +5,9 @@ describe 'The method race_winner:' do
     expect(race_winner).not_to be_nil
   end
 
+  it 'Is not empty' do
+    expect(race_winner).not_to eq('')
+  end
   it 'Returns a string' do
     expect(race_winner).to be_a String
   end

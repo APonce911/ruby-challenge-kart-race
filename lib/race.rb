a654e970dc8690fa52956dc835a85eeb5ec0e55e
Airton Ponce <airtonponce@outlook.com>
2019-01-18 19:31:51 -0200

    create race model

--- lib/race.rb
+++ lib/race.rb
@@ -1,17 +1,30 @@
+# this class will be responsable of creating all instances of Lap objects
 require 'csv'
+require_relative 'lap'
+class Race
+  def initialize
+    @laps = []
+    parse_data
+  end
 
-def race_winner
-  'airton'
+  attr_reader :laps
+
+  def race_winner
+    'airton'
+  end
+
+  private
+
+  def parse_data
+    file = './race.csv'
+    csv_options = { col_sep: ',', quote_char: '"', headers: :first_row.to_sym }
+    CSV.foreach(file, csv_options) { |row| @laps << { hour: row['hour'], cod: row['cod'].to_i, pilot: row['pilot'], lap_number: row['lap_number'], lap_time: row['lap_time'], lap_avg_time: row['lap_avg_time'] } }
+    @laps
+    # add_lap(@laps)
+  end
+
+# def add_lap
+#   p @laps.class
+
+# end
 end
-
-private
-
-def parse_data
-  file = './race.csv'
-  laps = []
-  csv_options = { col_sep: ',', quote_char: '"', headers: :first_row.to_sym }
-  CSV.foreach(file, csv_options) { |row| laps << { hour: row['hour'], cod: row['cod'].to_i, pilot: row['pilot'], lap_number: row['lap_number'], lap_time: row['lap_time'], lap_avg_time: row['lap_avg_time'] } }
-  laps
-end
-
-# p parse_data

217049f0d8c1526c6ecdc5ffc7ecd6ab87aff4dd
Airton Ponce <airtonponce@outlook.com>
2019-01-18 17:35:23 -0200

    test parse_data first element

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -22,7 +22,7 @@ describe 'The method parse_data:' do
   it 'Returns an array' do
     expect(parse_data).to be_a Array
   end
-  # it 'First element returns a hash' do
-  #   expect(parse_data[0]).to be_a Hash
-  # end
+  it 'First element returns a hash' do
+    expect(parse_data[0]).to be_a Hash
+  end
 end

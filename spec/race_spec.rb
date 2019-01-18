bbd599df0a859e68b94b3702ec5950d0355a3f25
Airton Ponce <airtonponce@outlook.com>
2019-01-18 20:03:44 -0200

    update tests to parse_data as private method on Race class

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -9,37 +9,39 @@ end
 
 
 describe 'The method race_winner:' do
+  race = Race.new
   it 'Is not nil' do
-    expect(race_winner).not_to be_nil
+    expect(race.race_winner).not_to be_nil
   end
   it 'Is not empty' do
-    expect(race_winner).not_to eq('')
+    expect(race.race_winner).not_to eq('')
   end
   it 'Returns a string' do
-    expect(race_winner).to be_a String
+    expect(race.race_winner).to be_a String
   end
 end
 
 describe 'The method parse_data:' do
+  race = Race.new
   it 'Is not nil' do
-    expect(parse_data).not_to be_nil
+    expect(race.send(:parse_data)).not_to be_nil
   end
   it 'Is not empty' do
-    expect(parse_data).not_to eq('')
+    expect(race.send(:parse_data)).not_to eq('')
   end
   it 'Returns an array' do
-    expect(parse_data).to be_a Array
+    expect(race.send(:parse_data)).to be_a Array
   end
   it 'Has 23 laps' do
-    expect(parse_data.size).to eq(23)
+    expect(race.send(:parse_data).size).to eq(23)
   end
   it 'First element returns a hash' do
-    expect(parse_data[0]).to be_a Hash
+    expect(race.send(:parse_data)[0]).to be_a Hash
   end
   it 'First element is not nil' do
-    expect(parse_data[0][:hour]).not_to be_nil
+    expect(race.send(:parse_data)[0][:hour]).not_to be_nil
   end
   it 'First element is not empty' do
-    expect(parse_data[0][:hour]).not_to eq('')
+    expect(race.send(:parse_data)[0][:hour]).not_to eq('')
   end
 end

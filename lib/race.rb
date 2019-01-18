9d9786aecb2d0446f1dce2dad2d3d62bceaf5e35
Airton Ponce <airtonponce@outlook.com>
2019-01-18 18:15:17 -0200

    adjust parse_data method to ignore headers

--- lib/race.rb
+++ lib/race.rb
@@ -9,7 +9,9 @@ private
 def parse_data
   file = './race.csv'
   laps = []
-  csv = { col_sep: ',', quote_char: '"', headers: %i[hour cod pilot lap_number lap_time lap_avg_time] }
-  CSV.foreach(file, csv) { |row| laps << { hour: row[:hour], cod: row[:cod].to_i, pilot: row[:pilot], lap_number: row[:lap_number], lap_time: row[:lap_time], lap_avg_time: row[:lap_avg_time] } }
+  csv_options = { col_sep: ',', quote_char: '"', headers: :first_row.to_sym }
+  CSV.foreach(file, csv_options) { |row| laps << { hour: row['hour'], cod: row['cod'].to_i, pilot: row['pilot'], lap_number: row['lap_number'], lap_time: row['lap_time'], lap_avg_time: row['lap_avg_time'] } }
   laps
 end
+
+# p parse_data

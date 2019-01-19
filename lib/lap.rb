8b444ef0fa13821e8fb33731579e3312840fefd5
Airton Ponce <airtonponce@outlook.com>
2019-01-19 08:18:49 -0200

    change string_to_seconds convertion method to lap model

--- lib/lap.rb
+++ lib/lap.rb
@@ -4,8 +4,16 @@ class Lap
     @cod = attributes[:cod]
     @pilot = attributes[:pilot]
     @lap_number = attributes[:lap_number]
-    @lap_time = attributes[:lap_time]
+    @lap_time = string_to_seconds(attributes[:lap_time])
   end
 
   attr_reader :cod, :pilot, :lap_number, :lap_time
+
+  private
+
+  def string_to_seconds(string)
+    min_to_sec = string[0].to_i * 60
+    mil_to_sec = string[5..7].to_f / 1000
+    string[2..3].to_f + mil_to_sec + min_to_sec
+  end
 end
--- lib/race.rb
+++ lib/race.rb
@@ -4,7 +4,6 @@ require_relative 'lap'
 # this class will be responsable of creating all instances of Lap objects
 class Race
   def initialize
-    # @laps = []
     parse_data
     create_lap_instances
   end
@@ -15,33 +14,25 @@ class Race
     'airton'
   end
 
-
   private
 
   def parse_data
-    @laps_hash = []
+    @laps_parsed = []
     file = './race.csv'
     csv_options = { col_sep: ',', quote_char: '"', headers: :first_row.to_sym }
-    CSV.foreach(file, csv_options) { |row| @laps_hash << { hour: row['hour'], cod: row['cod'].to_i, pilot: row['pilot'], lap_number: row['lap_number'], lap_time: row['lap_time'], lap_avg_time: row['lap_avg_time'] } }
-    @laps_hash
+    CSV.foreach(file, csv_options) { |row| @laps_parsed << { hour: row['hour'], cod: row['cod'].to_i, pilot: row['pilot'], lap_number: row['lap_number'], lap_time: row['lap_time'], lap_avg_time: row['lap_avg_time'] } }
+    @laps_parsed
   end
 
   def create_lap_instances
     @laps = []
-    @laps_hash.each do |lap|
-      seconds = string_to_seconds(lap[:lap_time])
-      properties = { cod: lap[:cod].to_i, pilot: lap[:pilot], lap_number: lap[:lap_number].to_i, lap_time: seconds }
+    @laps_parsed.each do |lap|
+      # seconds = string_to_seconds(lap[:lap_time])
+      properties = { cod: lap[:cod].to_i, pilot: lap[:pilot], lap_number: lap[:lap_number].to_i, lap_time: lap[:lap_time] }
       @laps << Lap.new(properties)
     end
-  end
-
-  def string_to_seconds(string)
-    min_to_sec = string[0].to_i * 60
-    mil_to_sec = string[5..7].to_f / 1000
-    string[2..3].to_f + mil_to_sec + min_to_sec
+    @laps
   end
 end
 
-r = Race.new
-p r.string_to_seconds('1:03.987')
-
+# r = Race.new

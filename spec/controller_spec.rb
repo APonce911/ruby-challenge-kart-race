8a647a4613e3efb4c94711cd0784b61d60c582d6
Airton Ponce <airtonponce@outlook.com>
2019-01-19 12:19:21 -0200

    define tests for seconds_to_string method

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -23,3 +23,24 @@ describe 'The method race_winner:' do
     expect(controller.race_winner).to be_a String
   end
 end
+
+describe 'The method seconds_to_string:' do
+  let(:race) { Race.new }
+  let(:controller) { Controller.new(race) }
+
+  it 'Is not nil' do
+    expect(controller.send(:seconds_to_string, 65.345)).not_to be_nil
+  end
+  it 'Is not empty' do
+    expect(controller.send(:seconds_to_string, 65.345)).not_to eq('')
+  end
+  it 'Returns a string' do
+    expect(controller.send(:seconds_to_string, 65.345)).to be_a String
+  end
+  it "Returns '1:05.345' with 65.345 seconds" do
+    expect(controller.send(:seconds_to_string, 65.345)).to eq('1:05.345')
+  end
+  it "Returns '1:03.745' with 63.745 seconds" do
+    expect(controller.send(:seconds_to_string, 63.745)).to eq('1:03.745')
+  end
+end

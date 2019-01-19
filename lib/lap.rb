1494462f37147ac2ff8ef2111506fbec8deb0883
Airton Ponce <airtonponce@outlook.com>
2019-01-19 16:37:36 -0200

    complete lap model with hour and lap_avg_pace

--- lib/lap.rb
+++ lib/lap.rb
@@ -1,13 +1,15 @@
 # each row of our DB will become an instance of the Lap model
 class Lap
   def initialize(attributes = {})
+    @hour = attributes[:hour]
     @cod = attributes[:cod]
     @pilot = attributes[:pilot]
     @lap_number = attributes[:lap_number]
     @lap_time = string_to_seconds(attributes[:lap_time])
+    @lap_avg_pace = attributes[:lap_avg_pace]
   end
 
-  attr_reader :cod, :pilot, :lap_number, :lap_time
+  attr_reader :hour, :cod, :pilot, :lap_number, :lap_time, :lap_avg_pace
 
   private
 

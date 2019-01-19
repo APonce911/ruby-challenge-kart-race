0b4640803ccda36a13f66dc4d77eaa5930f01210
Airton Ponce <airtonponce@outlook.com>
2019-01-19 12:55:15 -0200

    refactor code - create individual times method

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,7 +1,7 @@
 require_relative 'race'
 # The controller class will be responsible for calculating all results
 # TODO
-# Descobrir o vencedor da corrida
+# Descobrir o vencedor da corrida - DONE
 # Descobrir a melhor volta de cada piloto
 # Descobrir a melhor volta da corrida
 # Calcular a velocidade media de cada piloto durante toda corrida
@@ -10,19 +10,12 @@ class Controller
   # initialize(race) takes an instance of the Race as an argument.
   def initialize(race)
     @race = race
+    individual_times
   end
 
   def race_winner
-    individual_times = {}
-    @race.laps.each do |lap|
-      if individual_times.key?(lap.pilot.to_sym)
-        individual_times[lap.pilot.to_sym] << lap.lap_time
-      else
-        individual_times[lap.pilot.to_sym] = [lap.lap_time]
-      end
-    end
     ranking = []
-    individual_times.each do |laps|
+    @individual_times.each do |laps|
       if laps[1].size == 4
         pilot_total_time_array = [laps[0], laps[1].sum.truncate(3)]
         ranking << pilot_total_time_array
@@ -30,7 +23,7 @@ class Controller
     end
     ranking.sort_by! { |pilot_total_time_array| pilot_total_time_array[1] }
     winner = ranking[0][0].to_s
-    p winner_time = ranking[0][1]
+    # p winner_time = ranking[0][1]
     winner
   end
 
@@ -41,5 +34,16 @@ class Controller
     min = (seconds.to_i / 60).round(0)
     sec = (seconds.to_i - min * 60).round(0)
     min.to_s + ':' + ('0' if sec < 10) + sec.to_s + '.' + mil.to_s
+  end
+
+  def individual_times
+    @individual_times = {}
+    @race.laps.each do |lap|
+      if @individual_times.key?(lap.pilot.to_sym)
+        @individual_times[lap.pilot.to_sym] << lap.lap_time
+      else
+        @individual_times[lap.pilot.to_sym] = [lap.lap_time]
+      end
+    end
   end
 end

f401c29a8227fb1ef33d5737358410cc76194a27
Airton Ponce <airtonponce@outlook.com>
2019-01-19 16:07:50 -0200

    include hour and avg_pace on Lap instances

--- lib/race.rb
+++ lib/race.rb
@@ -14,23 +14,15 @@ class Race
     laps_parsed = []
     file = './race.csv'
     csv_options = { col_sep: ',', quote_char: '"', headers: :first_row.to_sym }
-    CSV.foreach(file, csv_options) { |row| laps_parsed << { hour: row['hour'], cod: row['cod'].to_i, pilot: row['pilot'], lap_number: row['lap_number'], lap_time: row['lap_time'], lap_avg_time: row['lap_avg_time'] } }
+    CSV.foreach(file, csv_options) { |row| laps_parsed << { hour: row['hour'], cod: row['cod'].to_i, pilot: row['pilot'], lap_number: row['lap_number'], lap_time: row['lap_time'], lap_avg_pace: row['lap_avg_pace'] } }
     laps_parsed
   end
 
   def create_lap_instances(laps_parsed)
     @laps = []
     laps_parsed.each do |lap|
-      # seconds = string_to_seconds(lap[:lap_time])
-      properties = { cod: lap[:cod].to_i, pilot: lap[:pilot], lap_number: lap[:lap_number].to_i, lap_time: lap[:lap_time] }
+      properties = {hour: lap[:hour], cod: lap[:cod].to_i, pilot: lap[:pilot], lap_number: lap[:lap_number].to_i, lap_time: lap[:lap_time],lap_avg_pace: lap[:lap_avg_pace] }
       @laps << Lap.new(properties)
     end
   end
 end
-
-# r = Race.new
-# p r
-# p r.class
-# p r.laps
-# p r.laps.class
-# p r.laps[0]

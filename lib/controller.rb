5c4d1cb1089ced2ef1903c9b0c9885e3ae5c8da8
Airton Ponce <airtonponce@outlook.com>
2019-01-19 09:38:51 -0200

    write controller #TO q q

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,11 @@
 require_relative 'race'
 # The controller class will be responsible for calculating all results
+# TODO
+# Descobrir o vencedor
+# Descobrir a melhor volta de cada piloto
+# Descobrir a melhor volta da corrida
+# Calcular a velocidade media de cada piloto durante toda corrida
+# Descobrir quanto tempo cada piloto chegou apos o vencedor
 class Controller
   # initialize(race) takes an instance of the Race as an argument.
   def initialize(race)

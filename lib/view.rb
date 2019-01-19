c6da03c96240b955148c2a62d17fdf0ca4e84498
Airton Ponce <airtonponce@outlook.com>
2019-01-19 17:05:12 -0200

    print best lap as a table

--- lib/view.rb
+++ lib/view.rb
@@ -2,6 +2,14 @@
 class View
   def print_winner(winner, time)
     # print %x{clear}
-    p "the winner is #{winner} on #{time} minutes."
+    puts "The winner is #{winner}. He completed 4 laps on #{time} minutes."
+  end
+  def print_best_lap(lap)
+    # print %x{clear}
+    puts 'The best lap is'
+    puts ''
+    # puts "Hour: #{lap.hour}; Pilot: #{lap.cod}-#{lap.pilot}; Lap Number: #{lap.lap_number}; Lap Time: #{lap.lap_time}; Lap avg pace: #{lap.lap_avg_pace};"
+    puts "Hour         | Pilot       | Lap Number| Lap Time| Lap avg pace"
+    puts "#{lap.hour} | #{lap.cod}-#{lap.pilot}  | #{lap.lap_number}         | #{lap.lap_time}  | #{lap.lap_avg_pace}"
   end
 end

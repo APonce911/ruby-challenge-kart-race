9c6b624340ac8553230d89ce32735666bf602fde
Airton Ponce <airtonponce@outlook.com>
2019-01-19 09:05:42 -0200

    controller initialize

---
+++ lib/controller.rb
@@ -0,0 +1,9 @@
+require_relative 'race'
+# The controller class will be responsible for calculating all results
+class Controller
+  # initialize(race) takes an instance of the Race as an argument.
+  def initialize(race)
+    @race = race
+  end
+
+end

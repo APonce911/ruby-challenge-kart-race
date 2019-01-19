b5005020184c524ab3e4363cd84a800bd487f865
Airton Ponce <airtonponce@outlook.com>
2019-01-19 08:19:18 -0200

    create string_to_seconds test

--- spec/lap_spec.rb
+++ spec/lap_spec.rb
@@ -7,3 +7,13 @@ describe 'Lap', :lap do
     expect(lap).to be_a(Lap)
   end
 end
+
+describe 'The method string_to_seconds:' do
+  lap = Lap.new(lap_time:'1:05.345')
+  it 'Gives a float' do
+    expect(lap.lap_time).to be_a Float
+  end
+  it 'To result in 65.345' do
+    expect(lap.lap_time).to eq(65.345)
+  end
+end
